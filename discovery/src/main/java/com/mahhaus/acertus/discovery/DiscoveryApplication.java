bf7ed2f886cde9d9d66185c820578b7f9e0ef99b
josias <mahhaus@gmail.com>
2019-05-05 21:27:12 -0300

    Iniciando Auth

--- discovery/src/main/java/com/mahhaus/acertus/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/mahhaus/acertus/discovery/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.mahhaus.acertus.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
